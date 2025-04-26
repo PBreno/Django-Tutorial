ee1d30778b3b7a04bd87a638faaf19fb5cdd82c1
PBreno <brenopombobp@hotmail.com>
2025-04-26 12:59:44 -0300

    Creation of category model, add the url and folder for media and adjusted the python's version

--- src/public/contact/admin.py
+++ src/public/contact/admin.py
@@ -4,4 +4,17 @@ from contact import models
 # Register your models here.
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    list_display = 'id', 'first_name', 'last_name', 'phone',
+    ordering = 'id',
+    search_fields = 'id', 'first_name', 'last_name',
+    list_per_page = 10
+    list_max_show_all = 20
+    list_editable = 'first_name', 'last_name',
+    list_display_links = 'id', 'phone',
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'id', 'name',
+    ordering = 'id',
+    search_fields = 'id', 'name',
