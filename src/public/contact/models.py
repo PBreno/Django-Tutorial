8fd62b3bb9b1365f9c3305f872168bf9979a65d5
PBreno <brenopombobp@hotmail.com>
2025-04-29 13:19:27 -0300

    create the faker contacts and adjusted the owner

--- src/public/contact/models.py
+++ src/public/contact/models.py
@@ -1,9 +1,15 @@
 from django.utils import timezone
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=50)
 
     def __str__(self):
@@ -19,6 +25,8 @@ class Contact(models.Model):
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/%d/')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
