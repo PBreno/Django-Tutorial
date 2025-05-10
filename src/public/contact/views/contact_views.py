d4fdfeb823fc9b16ba1b53391325bd81c6793776
PBreno <brenopombobp@hotmail.com>
2025-05-10 09:24:11 -0300

    Adjusting the searchbar

--- src/public/contact/views/contact_views.py
+++ src/public/contact/views/contact_views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
 from ..models import Contact
 # Create your views here.
 
@@ -8,6 +9,35 @@ def index(request):
     context = {
         'contacts': contacts,
         'site_title': 'Contactos -',
+    }
+
+    return render(request,
+                  'contact/index.html',
+                  context)
+
+def search(request):
+
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+
+    print(search_value)
+    contacts = Contact.objects\
+        .filter(show=True)\
+        .filter(
+            Q( first_name__icontains=search_value) |
+            Q( last_name__icontains=search_value) |
+            Q( email__icontains=search_value) |
+            Q( phone__icontains=search_value)
+        )\
+        .order_by('-id')
+
+    print(contacts.query)
+
+    context = {
+        'contacts': contacts,
+        'site_title': 'Search -',
     }
 
     return render(request,
