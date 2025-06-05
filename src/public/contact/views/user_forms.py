ee20b8af935ebe06f0ab2a0f25f8029fcc3cc818
PBreno <brenopombobp@hotmail.com>
2025-06-05 17:52:12 -0300

    updated user_forms, saved and validadation form

--- src/public/contact/views/user_forms.py
+++ src/public/contact/views/user_forms.py
@@ -7,6 +7,11 @@ def register(request):
 
     form = RegisterForm()
 
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+
     return render(
         request,
         'contact/register.html',
