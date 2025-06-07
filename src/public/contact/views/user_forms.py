002ab36bd573bfe9a07dd1aa0871af3ff2dc675d
PBreno <brenopombobp@hotmail.com>
2025-06-07 17:57:00 -0300

    Implementation of login and logout

--- src/public/contact/views/user_forms.py
+++ src/public/contact/views/user_forms.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.contrib import auth, messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, redirect
 
 from ..forms import RegisterForm
 
@@ -7,10 +9,14 @@ def register(request):
 
     form = RegisterForm()
 
+    #messages.info(request,'Um texto qualquer')
+
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Usuário registrado')
+            return redirect('contact:index')
 
     return render(
         request,
@@ -19,3 +25,35 @@ def register(request):
             'form': form,
         }
     )
+
+
+def login_view(request):
+
+    form = AuthenticationForm(request)
+
+    if request.method == 'POST':
+
+        form  = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            auth.login(request, user)
+            messages.success(request, 'Logado com sucesso!')
+
+            return redirect('contact:index')
+
+        messages.error(request, 'Login inválido')
+        
+    return render(
+        request,
+        'contact/login.html',
+        {
+            'form': form,
+        }
+    )
+
+
+def logout_view(request):
+    print('WE')
+    auth.logout(request)
+    return redirect('contact:login')
